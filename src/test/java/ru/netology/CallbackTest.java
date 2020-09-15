ee771a41bf725a8f0959028f5b64c92ca245b5de
Darya Morozova <fitosmd@yandex.ru>
2020-09-15 22:15:47 +0300

    Validation added

--- src/test/java/ru/netology/CallbackTest.java
+++ src/test/java/ru/netology/CallbackTest.java
@@ -1,25 +1,39 @@
 package ru.netology;
 
-import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.exactText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class CallbackTest {
 
     @Test
-    void shouldTestForm() {
+    void shouldTestCorrectForm() {
         open("http://localhost:9999/");
-        SelenideElement form = $("[action='/']");
-        form.$("[data-test-id='name']").setValue("Игорь Попов");
-        form.$("[data-test-id='phone']").setValue("+79875556565");
-        form.$("[data-test-id='agreement']").click();
-        form.$(".button__content").click();
-        form.$("[data-test-id='order-success']").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
-
-
+        $("[data-test-id=name] input").setValue("Игорь Попов");
+        $("[data-test-id='phone'] input").setValue("+79875556565");
+        $("[data-test-id='agreement']").click();
+        $(".button__content").click();
+        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 
+    @Test
+    void shouldTestNotCorrectName() {
+        open("http://localhost:9999/");
+        $("[data-test-id=name] input").setValue("Igor Popov");
+        $("[data-test-id='phone'] input").setValue("+79875556565");
+        $("[data-test-id='agreement']").click();
+        $(".button__content").click();
+        $(".input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+
+    @Test
+    void shouldTestNotCorrectPhone() {
+        open("http://localhost:9999/");
+        $("[data-test-id=name] input").setValue("Игорь Попов");
+        $("[data-test-id='phone'] input").setValue("2323535");
+        $("[data-test-id='agreement']").click();
+        $(".button__content").click();
+        $$(".input__sub").last().shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
 }
