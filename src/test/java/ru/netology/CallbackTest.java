3373333e38031f714571090ce0b6921546f2ba3d
Darya Morozova <fitosmd@yandex.ru>
2020-09-15 13:01:47 +0300

    Create test form

--- src/test/java/ru/netology/CallbackTest.java
+++ src/test/java/ru/netology/CallbackTest.java
@@ -1,5 +1,25 @@
 package ru.netology;
 
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
 public class CallbackTest {
+
+    @Test
+    void shouldTestForm() {
+        open("http://localhost:9999/");
+        SelenideElement form = $("[action='/']");
+        form.$("[data-test-id='name']").setValue("Игорь Попов");
+        form.$("[data-test-id='phone']").setValue("+79875556565");
+        form.$("[data-test-id='agreement']").click();
+        form.$(".button__content").click();
+        form.$("[data-test-id='order-success']").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+
+
+    }
 
 }
