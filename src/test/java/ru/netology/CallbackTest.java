57908fd0825a443cb678d141da7af315d5d6f49c
Darya Morozova <fitosmd@yandex.ru>
2020-09-21 12:22:11 +0300

    Negative tests with empty fields added

--- src/test/java/ru/netology/CallbackTest.java
+++ src/test/java/ru/netology/CallbackTest.java
@@ -24,7 +24,7 @@ public class CallbackTest {
         $("[data-test-id='phone'] input").setValue("+79875556565");
         $("[data-test-id='agreement']").click();
         $(".button__content").click();
-        $(".input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+        $("[data-test-id=name].input_invalid .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
     }
 
     @Test
@@ -34,6 +34,42 @@ public class CallbackTest {
         $("[data-test-id='phone'] input").setValue("2323535");
         $("[data-test-id='agreement']").click();
         $(".button__content").click();
-        $$(".input__sub").last().shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+        $("[data-test-id=phone].input_invalid .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
     }
+
+    @Test
+    void shouldTestEmptyName() {
+        open("http://localhost:9999/");
+        $("[data-test-id='phone'] input").setValue("+79875556565");
+        $("[data-test-id='agreement']").click();
+        $(".button__content").click();
+        $("[data-test-id=name].input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    void shouldTestEmptyPhone() {
+        open("http://localhost:9999/");
+        $("[data-test-id=name] input").setValue("Игорь Попов");
+        $("[data-test-id='agreement']").click();
+        $(".button__content").click();
+        $("[data-test-id=phone].input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    void shouldTestEmptyNameAndPhone() {
+        open("http://localhost:9999/");
+        $("[data-test-id='agreement']").click();
+        $(".button__content").click();
+        $("[data-test-id=name].input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    void shouldTestNotAgreement() {
+        open("http://localhost:9999/");
+        $("[data-test-id=name] input").setValue("Игорь Попов");
+        $("[data-test-id='phone'] input").setValue("+79875556565");
+        $(".button__content").click();
+        $("[data-test-id=agreement].input_invalid .checkbox__text").shouldHave(exactText("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй"));
+    }
+
 }
